ab83bf9399358d5a439a364253e13bd9f8ff66f3
shaghayegh <shfereydoonfar@gmail.com>
2019-06-12 21:19:37 +0430

    initial commit

---
+++ Sample13/app/src/main/java/com/test/sample1_3/Main3Activity.java
@@ -0,0 +1,20 @@
+package com.test.sample1_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void signUp(View view) {
+    }
+
+    public void signIn(View view) {
+    }
+}
---
+++ sample1-2/app/src/main/java/com/test/sample1_2/Main3Activity.java
@@ -0,0 +1,20 @@
+package com.test.sample1_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void redemption(View view) {
+    }
+
+    public void guidance(View view) {
+    }
+}
